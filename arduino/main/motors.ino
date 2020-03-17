9d602d236d12d675b37880be657c9108beca3dfd
ikheyfets <44104569+ikheyfets@users.noreply.github.com>
2020-03-17 01:53:57 -0400

    Update motors.ino

--- arduino/main/motors.ino
+++ arduino/main/motors.ino
@@ -95,7 +95,7 @@ void motorDrive(boolean motor, boolean dir, int mSpeed) {
 
   boolean pinIn1;
   if (dir == turnCW) {
-    pinIn1 = HIGH;
+    pinIn1 = LOW;
   } else {
     pinIn1 = LOW;
   }
@@ -133,13 +133,13 @@ void continuousMotorDrive(boolean motor, boolean dir, int mSpeed) {
 
 void motorContiuousForward(int mSpeed){
  //Do a tight turn towards motor1: Motor2 forward, Motor1 reverse
-  motorDrive(motor1, turnCW, 10);
+  motorDrive(motor1, turnCCW, 100);
   motorDrive(motor2, turnCW, 100);
 }
 
 void motorForward(int mSpeed, int duration) {
   //Do a tight turn towards motor1: Motor2 forward, Motor1 reverse
-  motorDrive(motor1, turnCW, 10);
+  motorDrive(motor1, turnCW, 100);
   motorDrive(motor2, turnCW, 100);
   delay(duration);
   motorBrake();
