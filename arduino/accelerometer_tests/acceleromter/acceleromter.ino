914d86d34724c924f6b21412f4cdff05e80801b0
ikheyfets <44104569+ikheyfets@users.noreply.github.com>
2020-08-12 23:03:57 -0400

    Update acceleromter.ino

--- arduino/accelerometer_tests/acceleromter/acceleromter.ino
+++ arduino/accelerometer_tests/acceleromter/acceleromter.ino
@@ -3,7 +3,12 @@
 
 MMA8452Q accel;                   // create instance of the MMA8452 class
 
+int interval = 250;
+
 int time;
+
+float deltaV;
+int v = -1;
 
 void setup() {
   
@@ -22,12 +27,23 @@ void loop() {
   if (accel.available()) {      // Wait for new data from accelerometer
     // We need acceleration in Y direction in terms of g units
     time = millis();
+    // Attempt to calculate velocity
+    deltaV = accel.getCalculatedY() * (interval);
+    if (v >= 0) {
+      v = v + deltaV;
+    } else {
+      v = 0;
+    }
+
+    //Printing data
+    delay(interval);
     Serial.print(accel.getCalculatedY(), 3);
     Serial.print('\t');
     Serial.print(time);
-    delay(50);
+    Serial.print('\t');
+    Serial.print(v);
     Serial.println();
-    // Attempt to calculate velocity
+    
     
   }
 }
