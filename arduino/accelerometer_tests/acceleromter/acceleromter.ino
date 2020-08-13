d44aff864a60722ac8c301a6417c220f7e731bdf
ikheyfets <44104569+ikheyfets@users.noreply.github.com>
2020-08-12 23:45:25 -0400

    Update acceleromter.ino

--- arduino/accelerometer_tests/acceleromter/acceleromter.ino
+++ arduino/accelerometer_tests/acceleromter/acceleromter.ino
@@ -3,7 +3,8 @@
 
 MMA8452Q accel;                   // create instance of the MMA8452 class
 
-int interval = 250;
+int interval = 500;
+float acc;
 
 int time;
 
@@ -21,29 +22,35 @@ void setup() {
     Serial.println("Not Connected. Please check connections and read the hookup guide.");
     while (1);
   }
+  accel.init(); // Default init: +/-2g and 800Hz ODR
 }
+
+float StartTime = millis();
+float CurrentTime;
+float ElapsedTime;
+float velocity = 0;
+
 
 void loop() {
   if (accel.available()) {      // Wait for new data from accelerometer
     // We need acceleration in Y direction in terms of g units
-    time = millis();
+    acc = accel.getCalculatedY();
+    acc = acc / 64 * 980; //converts to cm/s^2
+    CurrentTime = millis();
+    ElapsedTime = CurrentTime - StartTime;
     // Attempt to calculate velocity
-    deltaV = accel.getCalculatedY() * (interval);
-    if (v >= 0) {
-      v = v + deltaV;
-    } else {
-      v = 0;
+    if (abs(acc) > 1) {
+      velocity += acc *(ElapsedTime/1000);
     }
-
     //Printing data
     delay(interval);
-    Serial.print(accel.getCalculatedY(), 3);
+    Serial.print(acc);
     Serial.print('\t');
-    Serial.print(time);
+    Serial.print(ElapsedTime);
     Serial.print('\t');
-    Serial.print(v);
+    Serial.print(velocity);
     Serial.println();
-    
+    StartTime = CurrentTime;
     
   }
 }
